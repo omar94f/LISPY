1653ce07cc469fc3b08808a1ec8f26e3a2bbf9fe
omar <omar94f@gmail.com>
2021-12-12 12:30:14 +0100

    Preprocessor directives added in prompt.c, gitignore modified

--- source/prompt.c
+++ source/prompt.c
@@ -1,8 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// __APPLE__ for mac
+#ifdef _WIN32
+
+#include <string.h>
+
+static char buffer[2048];
+
+char* readline(char *prompt){
+    fputs(prompt, stdout);
+    fgets(buffer, 2048, stdin);
+
+    char *cpy = malloc(strlen(buffer)+1);
+    strcpy(cyp, buffer);
+    coy[strlen(cpy)-1] = '\0';
+
+    return cpy;
+}
+
+void add_history(char *unused) {}
+
+#else
+
 #include <editline/readline.h>
 
+#endif
 
 
 int main(int arc, char** argv) {
