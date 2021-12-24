e4230250ac3ae921d195abde1ba98cd9f6884d5f
omar <omar94f@gmail.com>
2021-12-24 18:13:22 +0100

    language parser added for polish notation

--- source/parsing.c
+++ source/parsing.c
@@ -30,6 +30,21 @@ void add_history(char *unused) {}
 
 
 int main(int arc, char** argv) {
+    /* create some parsers */
+    mpc_parser_t* Number = mpc_new("number");
+    mpc_parser_t* Operator = mpc_new("operator");
+    mpc_parser_t* Expr = mpc_new("expr");
+    mpc_parser_t* Lispy = mpc_new("lispy");
+
+    /* Define them with the following language */
+    mpca_lang(MPCA_LANG_DEFAULT,
+    "  \
+      number: /-?[0-9]+/;                                   \
+      operator:  '+' | '*' |  '/' | '-' ;                   \
+      expr: <number> | '(' <operator> <expr>+ ')' ;         \
+      lispy: /^/ <operator> <expr>+ /$/;  \
+    ", Number, Operator, Expr, Lispy);
+
     fputs("Lispy Version 0.0.0.0.1\n", stdout);
     fputs("Press Ctrl+c to Exit\n", stdout);
 
@@ -39,11 +54,20 @@ int main(int arc, char** argv) {
 
         add_history(input);
 
-        printf("No you're a %s\n", input);
+        // printf("No you're a %s\n", input);
+        mpc_result_t r;
+        if(mpc_parse("<stdin>", input, Lispy, &r)){
+            mpc_ast_print(r.output);
+            mpc_ast_delete(r.output);
+        } else {
+            mpc_err_print(r.error);
+            mpc_err_delete(r.error);
+        }
 
         free(input);
     }
     
-
+    /* Undefine and delete parser */
+    mpc_cleanup(4, Number, Operator, Expr, Lispy);
     return 0;
 }
